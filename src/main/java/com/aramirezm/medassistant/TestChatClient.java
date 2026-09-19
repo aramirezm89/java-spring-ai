6eefab96beaf00dd09343e1d373ce3e95c09c65a
aramirezm <aramirez.unimarc@gmail.com>
2026-09-19 14:22:27 -0300

    test: test ai model con ChatClient

---
+++ src/main/java/com/aramirezm/medassistant/TestChatClient.java
@@ -0,0 +1,31 @@
+package com.aramirezm.medassistant;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TestChatClient implements CommandLineRunner {
+
+    private final ChatClient chatClient;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        String response = chatClient.prompt("breve descripcion zelda majoras mask")
+                .call()
+                .content();
+
+        log.info(response);
+
+    }
+}
--- src/main/java/com/aramirezm/medassistant/TestLlmCall.java
+++ src/main/java/com/aramirezm/medassistant/TestLlmCall.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 @Slf4j
 public class TestLlmCall implements CommandLineRunner {
